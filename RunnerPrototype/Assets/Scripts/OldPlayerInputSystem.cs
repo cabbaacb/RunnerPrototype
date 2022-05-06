15a472cd19a84335d0f94d33f6bd9fa477a9c18a
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-05-06 20:15:08 +0300

    playerMoving

---
+++ RunnerPrototype/Assets/Scripts/OldPlayerInputSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldPlayerInputSystem : PlayerBehaviour
+{
+    private void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump")) Jump();
+        
+
+    }
+}
---
+++ RunnerPrototype/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] [Min(1f)] private float speedForward = 5f;
+    [SerializeField] [Min(5f)] private float speedJump = 10f;
+
+    [SerializeField] private Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Vector3 movingForward = transform.forward * speedForward * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movingForward);
+    }
+
+    protected void Jump()
+    {
+        rb.AddForce(transform.up * speedJump, ForceMode.Impulse);
+    }
+
+
+
+
+
+
+
+
+
+    /*
+    [SerializeField][Min(1f)] private float speedForward = 5f;
+    [SerializeField][Min(1f)] private float speedJump = 1f;
+
+    private Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        StartCoroutine(MovingForward());
+    }
+
+    protected void Jump()
+    {
+        rigidBody.AddForce(transform.up * speedJump, ForceMode.Impulse);
+    }
+    private IEnumerator MovingForward()
+    {
+        while(true)
+        {
+            rigidBody.velocity += Vector3.forward * speedForward * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+            
+            //transform.position += Vector3.forward * speedForward * Time.deltaTime;
+
+            //yield return null;
+        }
+    }*/
+}
