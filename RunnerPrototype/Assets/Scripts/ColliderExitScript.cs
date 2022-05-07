ae33b1beda594647ff93e37801f521bbe2c87141
MalacayHooves <malacayhooves@gmail.com>
2022-05-07 14:53:23 +0500

    дорожка
    Запилил спавнящуюся дорожку. Когда персонаж двигается вперёд, она постепенно добавляется спереди, так что можно бежать до бесконечности.
    Делал всё в другой сцене, так что в первой можно ставить какие-нибудь эксперименты, если нужно.

---
+++ RunnerPrototype/Assets/Scripts/ColliderExitScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderExitScript : MonoBehaviour
+{
+    private LevelManager _levelManager;
+
+    private void Start()
+    {
+        _levelManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelManager>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_levelManager.RowDistance < Mathf.RoundToInt(gameObject.transform.position.z))
+        {
+            _levelManager.RowDistance = Mathf.RoundToInt(gameObject.transform.position.z);
+        }
+    }
+}
---
+++ RunnerPrototype/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _groundTile;
+
+    [SerializeField] private int _numberOfLines = 5;
+    private Vector3 _spawnRow;
+
+    private int _rowDistance = 2;
+
+    public int RowDistance
+    {
+        get { return _rowDistance; }
+        set { _rowDistance = value; ExitFromCollider(); }
+    }
+
+
+    private void Start()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            SpawnRow();
+        }
+        
+    }
+
+    private void SpawnRow()
+    {
+        Vector3 spawnPoint = _spawnRow;
+        for (int i = 0; i < _numberOfLines; i++)
+        {
+            if (i == 0)
+            {
+                SpawnTile(spawnPoint, true);
+            }
+            else
+            {
+                SpawnTile(spawnPoint, false);
+            }
+            spawnPoint.x += 2;
+        }
+    }
+
+    private void SpawnTile(Vector3 position, bool setNextRow)
+    {
+        GameObject tile = Instantiate(_groundTile, position, Quaternion.identity, gameObject.transform);
+        if (setNextRow)
+        {
+            _spawnRow = tile.transform.GetChild(1).transform.position;
+        }
+    }
+
+    private void ExitFromCollider()
+    {
+        SpawnRow();
+    }
+}
